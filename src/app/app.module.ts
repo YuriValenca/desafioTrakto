4c0ec8a711b39be82dd02e46b59d83cfa42e9573
Yuri Valença <yvc@cin.ufpe.br>
2023-04-24 15:15:39 -0300

    refactor: :hammer: Change existing code into a correct component
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
