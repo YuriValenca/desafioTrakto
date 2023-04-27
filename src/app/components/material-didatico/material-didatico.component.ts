9caa4acc5faf73a187a724b8fe5c3194546aab63
Yuri Valença <yvc@cin.ufpe.br>
2023-04-27 18:52:13 -0300

    feature: :sparkles: Create and style material-didatico component
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/app/components/material-didatico/material-didatico.component.ts
+++ src/app/components/material-didatico/material-didatico.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SlidesService } from '../../services/slide.service';
 
 @Component({
   selector: 'app-material-didatico',
   templateUrl: './material-didatico.component.html',
   styleUrls: ['./material-didatico.component.scss']
 })
-export class MaterialDidaticoComponent {
+export class MaterialDidaticoComponent implements OnInit {
+  slides: { id: string, imageUrl: string, alt: string, title: string }[] = [];
 
+  constructor(private slidesService: SlidesService) {
+    this.slides = this.slidesService.getSlides();
+  }
+
+  goToDocument(documentId: string) {
+    window.open(`https://editor.trakto.io/presentation/p/${documentId}`, '_blank');
+  }
+
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
 }
