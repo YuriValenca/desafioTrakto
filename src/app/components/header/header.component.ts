5a8e7b8dcd3c2ab9f6f064e8a983d7118edffebc
Yuri Valença <yvc@cin.ufpe.br>
2023-04-27 17:59:32 -0300

    fix: :hammer: Add function to header button
    Signed-off-by: Yuri Valença <yvc@cin.ufpe.br>

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,10 @@ import { Component, Input } from '@angular/core';
 export class HeaderComponent {
   @Input() dark: boolean = true;
   currentDate: Date = new Date();
+
+  constructor(private router: Router) { }
+
+  changeEnv() {
+    this.router.navigate(['/modules']);
+  }
 }
